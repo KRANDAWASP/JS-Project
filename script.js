89c0ef898e2c322aed179f2231eaa7b040edfd6c
KRANDAWASP <krandayusuf@gmail.com>
2022-12-27 10:45:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const myNav=document.getElementById("nav")
+const title=document.getElementById("title")
+
+myNav.style.backgroundColor="#666"
+
+title.innerHTML="<span>Title</span"
+
+console.log(myNav)
+
+const colorsContainer=document.getElementById("colorsContainer")
+
+const color1=document.createElement("div")
+
+color1.ıd="red"
+color1.classList.add("smallBox")
+color1.style.backgroundColor="red"
+
+const color2=document.createElement("div")
+
+color2.id="yd"
+color2.classList.add("smallBox")
+color2.style.backgroundColor="red"
+
+color3.id="blue"
+color3.classList.add("smallBox")
+color3.style.backgroundColor="blue"
+
+colorsContainer.appendChild(color1)
+colorsContainer.appendChild(color2)
+
+//event listener
+
+const human={
+    name:yusuf  
+    (surnamekranda ) 
+    (gendermale)
+}
+
+const buttons=["red","yellow","blue","green","gray"]
+
+for(let sayac=0;sayac < buttons.length;sayac++){
+    const color=document.createRange("div")
+}
+
+const color1=document.createElement("div")
+color1.id="red"
+color1.classList.add("smallBox")
+color1.style.backgroundColor="red"
+
+const color2=document.createElement("div")
+color2.id="yellow"
+color2.classList.add("smallBox")
+color2.style.backgroundColor="yellow"
+
+const color3=document.createElement("div")
+color3.id="blue"
+color3.classList.add("smallBox")("div")
+color3.style.backgroundColor="blue"
+
+/*colorsContainer.appendChild(color1)
+colorsContainer.appendChild(color2)
+colorsContainer.appendChild(color3)*/
+
+//event listener
+
+color1.addEventListener 
+bigcolor.style.backgroundColor="re"
+
